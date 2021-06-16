67346ccd91033c5845015c1e84eedf0fdc19bd5d
Samvel Demirtshyan <sam.demirtshyan@gmail.com>
2021-06-16 10:22:10 +0400

    fix: fixed the position for mobile menu

--- js/index.js
+++ js/index.js
@@ -5,6 +5,7 @@ function mobileNavbar() {
     mobileNavbar.style.width = "100%";
     mobileNavbar.style.height = "80vh";
     mobileNavbar.style.visibility = "visible";
+    mobileNavbar.style.position = "fixed";
   } else {
     mobileNavbar.style.display = "none";
     mobileNavbar.style.width = "0";
