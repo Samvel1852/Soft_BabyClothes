03f5ccda098a8b0601fb2dad3f203734f2dcf4f5
Samvel Demirtshyan <sam.demirtshyan@gmail.com>
2021-06-17 17:18:34 +0400

    feat: Added slider in home page hotSale section

--- js/index.js
+++ js/index.js
@@ -1,4 +1,8 @@
-function mobileNavbar() {
+"use strict";
+// Burger Menu
+let menuIcon = document.getElementById("menuIconId");
+
+menuIcon.addEventListener("click", function mobileNavbar() {
   let mobileNavbar = document.querySelector(".mobileMenu");
   if (!mobileNavbar.style.display || mobileNavbar.style.display === "none") {
     mobileNavbar.style.display = "flex";
@@ -12,4 +16,49 @@ function mobileNavbar() {
     mobileNavbar.style.height = "0";
     mobileNavbar.style.visibility = "hidden";
   }
-}
+});
+
+//Slider
+
+let prevSlide = document.getElementById("prevBtn");
+let nextSlide = document.getElementById("nextBtn");
+let count = 0;
+let imgs = [
+  ["./images/homeWebp.webp", "./images/homeWebp1.webp"],
+  ["./images/homeWebp2.webp", "./images/homeWebp3.webp"],
+  ["./images/homeWebp4.webp", "./images/homeWebp5.webp"],
+];
+let img1 = document.getElementById("hotSale1");
+let img2 = document.getElementById("hotSale2");
+
+let hotSaleSlide = function () {
+  if (count === imgs.length - 1) {
+    count = 0;
+  } else {
+    count += 1;
+  }
+  [img1.src, img2.src] = imgs[count];
+};
+
+let prevHotSaleSlide = function () {
+  if (count === 0) {
+    count = imgs.length - 1;
+  } else {
+    count -= 1;
+  }
+  [img1.src, img2.src] = imgs[count];
+};
+
+let intervalSlide = setInterval(() => {
+  if (count === imgs.length - 1) {
+    count = 0;
+  } else {
+    count += 1;
+  }
+  [img1.src, img2.src] = imgs[count];
+}, 6000);
+
+prevSlide.addEventListener("click", prevHotSaleSlide);
+nextSlide.addEventListener("click", hotSaleSlide);
+prevSlide.addEventListener("click", intervalSlide);
+nextSlide.addEventListener("click", intervalSlide);
